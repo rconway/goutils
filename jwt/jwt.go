3ca7373395d6cd0b09fd73a54f589570a470776a
Richard Conway <richard@rconway.co.uk>
2020-09-24 08:38:08 +0100

    jwt: better error handling

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -5,15 +5,19 @@ import (
 	"crypto/sha256"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
+	"strings"
+
+	"github.com/pkg/errors"
 )
 
 // EncodeJSON encodes the supplied JSON object tree as a JSON string
-func EncodeJSON(j map[string]interface{}) []byte {
+func EncodeJSON(j map[string]interface{}) ([]byte, error) {
 	b, err := json.Marshal(j)
 	if err != nil {
-		return nil
+		return nil, errors.Wrap(err, "failed to marshal bytes to json")
 	}
-	return b
+	return b, nil
 }
 
 // Base64Encode encodes the supplied data to Base64 string
@@ -31,20 +35,82 @@ func Base64Decode(b64 string) []byte {
 }
 
 // MakeToken creates a JWT with HS256 signature from the supplied claims
-func MakeToken(claims map[string]interface{}, key string) string {
+func MakeToken(claims map[string]interface{}, key string) (string, error) {
 	// header
 	header := make(map[string]interface{})
 	header["alg"] = "HS256"
 	header["typ"] = "JWT"
 
 	// signature
-	message := Base64Encode(EncodeJSON(header)) + "." + Base64Encode(EncodeJSON(claims))
-
-	mac := hmac.New(sha256.New, []byte(key))
-	mac.Write([]byte(message))
-	signature := mac.Sum(nil)
+	headerJSON, err := EncodeJSON(header)
+	if err != nil {
+		return "", errors.Wrap(err, "could not create header JSON")
+	}
+	claimsJSON, err := EncodeJSON(claims)
+	if err != nil {
+		return "", errors.Wrap(err, "could not create claims JSON")
+	}
+	b64Header := Base64Encode(headerJSON)
+	b64Claims := Base64Encode(claimsJSON)
+	signature := Signature(b64Header, b64Claims, key)
 
 	// token
-	token := message + "." + Base64Encode(signature)
-	return token
+	token := b64Header + "." + b64Claims + "." + Base64Encode(signature)
+	return token, nil
+}
+
+// Signature generates the signature of the base64 encoded header+claims with the supplied key
+func Signature(b64Header string, b64Claims string, key string) []byte {
+	message := b64Header + "." + b64Claims
+	mac := hmac.New(sha256.New, []byte(key))
+	mac.Write([]byte(message))
+	return mac.Sum(nil)
+}
+
+// MapToStruct converts a map[string]interface{} to the supplied struct type
+func MapToStruct(m map[string]interface{}, v interface{}) error {
+	b, err := json.Marshal(m)
+	if err != nil {
+		return errors.Wrap(err, "failed to marshal map to json")
+	}
+	err = json.Unmarshal(b, v)
+	if err != nil {
+		return errors.Wrap(err, "failed to unmarshal struct from json")
+	}
+	return nil
+}
+
+// GetClaims return the claims (map) from the supplied JWT string
+func GetClaims(token string, key string) (map[string]interface{}, error) {
+	// Get parts
+	parts := strings.Split(token, ".")
+	if len(parts) != 3 {
+		return nil, errors.New(fmt.Sprintf("wrong number of JWT parts: expected 3, got %v", len(parts)))
+	}
+
+	// header
+	headerBytes := Base64Decode(parts[0])
+	header := make(map[string]interface{})
+	err := json.Unmarshal(headerBytes, &header)
+	if err != nil {
+		return nil, errors.Wrap(err, "problem with JWT header")
+	}
+	if header["alg"] != "HS256" || header["typ"] != "JWT" {
+		return nil, errors.New("only support JWTs with signature of type HS256")
+	}
+
+	// claims
+	claimsBytes := Base64Decode(parts[1])
+	claims := make(map[string]interface{})
+	err = json.Unmarshal(claimsBytes, &claims)
+	if err != nil {
+		return nil, errors.Wrap(err, "problem with JWT claims")
+	}
+
+	// signature check
+	if Base64Encode(Signature(parts[0], parts[1], key)) != parts[2] {
+		return nil, errors.New("invalid signature")
+	}
+
+	return claims, nil
 }
--- jwt/jwt_test.go
+++ jwt/jwt_test.go
@@ -1,19 +1,32 @@
 package jwt
 
 import (
-	"encoding/json"
-	"strings"
+	"fmt"
+	"os"
 	"testing"
 )
 
 const EncodedJSON = "{\"alg\":\"HS256\",\"typ\":\"JWT\"}"
 const ExpectedJWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhZGRyZXNzIjoiU3RvdGZvbGQiLCJ1c2VybmFtZSI6InJjb253YXkifQ.flmSqrq09bwTQvylFtBo4gzr-ZGmDqUtJDCLU60rIms"
+const Key = "fredbob"
+
+var ExampleClaims map[string]interface{}
+
+func TestMain(m *testing.M) {
+	ExampleClaims = make(map[string]interface{})
+	ExampleClaims["username"] = "rconway"
+	ExampleClaims["address"] = "Stotfold"
+	os.Exit(m.Run())
+}
 
 func TestEncodeJSON(t *testing.T) {
 	j := make(map[string]interface{})
 	j["alg"] = "HS256"
 	j["typ"] = "JWT"
-	jbytes := EncodeJSON(j)
+	jbytes, err := EncodeJSON(j)
+	if err != nil {
+		t.Error(err)
+	}
 	jstr := string(jbytes)
 	t.Log("json =", jstr)
 	if string(jstr) != EncodedJSON {
@@ -28,11 +41,12 @@ func TestBase64Encode(t *testing.T) {
 
 func TestMakeToken(t *testing.T) {
 	// claims
-	claims := make(map[string]interface{})
-	claims["username"] = "rconway"
-	claims["address"] = "Stotfold"
+	claims := ExampleClaims
 
-	token := MakeToken(claims, "fredbob")
+	token, err := MakeToken(claims, Key)
+	if err != nil {
+		t.Error(err)
+	}
 
 	t.Log("token =", token)
 	if token != ExpectedJWT {
@@ -41,34 +55,14 @@ func TestMakeToken(t *testing.T) {
 }
 
 func TestGetClaims(t *testing.T) {
-	token := ExpectedJWT
-
-	parts := strings.Split(token, ".")
-
-	// h := parts[0]
-	c := parts[1]
-	// s := parts[2]
-
-	cb := Base64Decode(c)
-	claims := make(map[string]interface{})
-	err := json.Unmarshal(cb, &claims)
+	claims, err := GetClaims(ExpectedJWT, Key)
 	if err != nil {
-		t.Error("could not unmarshal claims")
-	} else {
-		t.Log("claims =", claims)
+		t.Error("error getting claims from token")
 	}
 
-	// convert claims to a structure
-	userClaims := struct {
-		Username string
-		Address  string
-	}{}
-	b, _ := json.Marshal(claims)
-	t.Log("b =", string(b))
-	err = json.Unmarshal(b, &userClaims)
-	if err != nil {
-		t.Error(err.Error())
-	} else {
-		t.Log("userClaims =", userClaims)
+	if claims["username"] != "rconway" {
+		t.Error(fmt.Sprintf("wrong username: expected rconway, got %v", claims["username"]))
 	}
+
+	t.Log("claims =", claims)
 }
