5169da450d67b6221626b393c58b4deec158f807
Richard Conway <richard@rconway.co.uk>
2020-10-31 13:49:57 +0000

    [request logger] Allow listening port to be set by environment variable

--- requestlogger/requestlogger.go
+++ requestlogger/requestlogger.go
@@ -5,22 +5,47 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 )
 
+var defaultPort uint16 = 80
+
 func logger(next http.Handler) http.Handler {
 	return handlers.CombinedLoggingHandler(os.Stdout, next)
 }
 
+func getPort() (port uint16) {
+	portEnv, ok := os.LookupEnv("PORT")
+	if ok {
+		portVal, err := strconv.ParseUint(portEnv, 10, 16)
+		if err != nil {
+			port = defaultPort
+			log.Println("WARNING: Bad PORT specified. Using default port =", port)
+		} else {
+			port = uint16(portVal)
+			log.Println("Using specified port =", port)
+		}
+	} else {
+		port = defaultPort
+		log.Println("Using default port =", port)
+	}
+	return port
+}
+
 func main() {
-	fmt.Println("request logger")
+
+	PORT := getPort()
+	log.Println("request logger running on port:", PORT)
 
 	mux := mux.NewRouter()
 	mux.Use(logger)
 
 	mux.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Host
+		fmt.Fprintln(w, "Host:", r.Host)
 		// URL
 		fmt.Fprintln(w, "URL:", r.URL)
 		// Headers
@@ -39,5 +64,7 @@ func main() {
 		}
 	})
 
-	log.Fatal(http.ListenAndServe(":80", mux))
+	http.Handle("/", mux)
+
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", PORT), nil))
 }
