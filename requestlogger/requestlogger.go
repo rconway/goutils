a4535b93134a98030485fb9c78d441c9e11536c7
Richard Conway <richard@rconway.co.uk>
2020-11-04 08:33:42 +0000

    [request-logger] Revert to simple implementation

--- requestlogger/requestlogger.go
+++ requestlogger/requestlogger.go
@@ -9,7 +9,6 @@ import (
 	"strconv"
 
 	"github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
 )
 
 var defaultPort uint16 = 80
@@ -36,43 +35,46 @@ func getPort() (port uint16) {
 	return port
 }
 
-func main() {
+type summaryHandler struct{}
 
+func (summaryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Host
+	fmt.Fprintln(w, "Host:", r.Host)
+	// URL
+	fmt.Fprintln(w, "URL:", r.URL)
+	// Method
+	fmt.Fprintln(w, "Method:", r.Method)
+	// Headers
+	fmt.Fprintln(w, "Headers:")
+	for headerKey, headerVal := range r.Header {
+		fmt.Fprintf(w, "  %v: %v\n", headerKey, headerVal)
+	}
+	// Query params...
+	fmt.Fprintln(w, "Params:")
+	for paramKey, paramVal := range r.URL.Query() {
+		fmt.Fprint(w, "  ", paramKey, ":")
+		for _, paramValItem := range paramVal {
+			fmt.Fprint(w, " ", paramValItem)
+		}
+		fmt.Fprintln(w)
+	}
+	// Body
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("ERROR reading request body:", err)
+	} else {
+		fmt.Fprintln(w, "Body:", string(data))
+	}
+}
+
+func main() {
 	PORT := getPort()
 	log.Println("request logger running on port:", PORT)
 
-	mux := mux.NewRouter().SkipClean(true)
-	mux.Use(logger)
+	// Create handler chain
+	var handler http.Handler
+	handler = summaryHandler{}
+	handler = logger(handler)
 
-	mux.PathPrefix("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Host
-		fmt.Fprintln(w, "Host:", r.Host)
-		// URL
-		fmt.Fprintln(w, "URL:", r.URL)
-		// Method
-		fmt.Fprintln(w, "Method:", r.Method)
-		// Headers
-		fmt.Fprintln(w, "Headers:")
-		for headerKey, headerVal := range r.Header {
-			fmt.Fprintf(w, "  %v: %v\n", headerKey, headerVal)
-		}
-		// Query params...
-		fmt.Fprintln(w, "Params:")
-		for paramKey, paramVal := range r.URL.Query() {
-			fmt.Fprint(w, "  ", paramKey, ":")
-			for _, paramValItem := range paramVal {
-				fmt.Fprint(w, " ", paramValItem)
-			}
-			fmt.Fprintln(w)
-		}
-		// Body
-		data, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println("ERROR reading request body:", err)
-		} else {
-			fmt.Fprintln(w, "Body:", string(data))
-		}
-	})
-
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", PORT), mux))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", PORT), handler))
 }
