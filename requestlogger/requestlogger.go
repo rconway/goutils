8d9e4a39ba6c191a8c21babbf0b16c5f00f76012
Richard Conway <richard@rconway.co.uk>
2021-06-18 16:17:17 +0100

    Update to go version 1.16 and ensure httputils.DumpRequest() is used where possible

--- requestlogger/requestlogger.go
+++ requestlogger/requestlogger.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 
 	"github.com/gorilla/handlers"
+	"github.com/rconway/goutils/httputils"
 )
 
 var defaultPort uint16 = 80
@@ -37,68 +37,14 @@ func getPort() (port uint16) {
 
 func myHandler() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// RequestURI
-		fmt.Fprintln(w, "RequestURI:", r.RequestURI)
-		// Host
-		fmt.Fprintln(w, "Host:", r.Host)
-		// URL
-		fmt.Fprintln(w, "URL:", r.URL)
-		// Method
-		fmt.Fprintln(w, "Method:", r.Method)
-		// Headers
-		fmt.Fprintln(w, "Headers:")
-		for headerKey, headerVal := range r.Header {
-			fmt.Fprintf(w, "  %v: %v\n", headerKey, headerVal)
-		}
-		// Query params...
-		fmt.Fprintln(w, "Params:")
-		for paramKey, paramVal := range r.URL.Query() {
-			fmt.Fprint(w, "  ", paramKey, ":")
-			for _, paramValItem := range paramVal {
-				fmt.Fprint(w, " ", paramValItem)
-			}
-			fmt.Fprintln(w)
-		}
-		// Body
-		data, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			log.Println("ERROR reading request body:", err)
-		} else {
-			fmt.Fprintln(w, "Body:", string(data))
-		}
+		httputils.DumpRequest(w, r)
 	})
 }
 
 type summaryHandler struct{}
 
 func (summaryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Host
-	fmt.Fprintln(w, "Host:", r.Host)
-	// URL
-	fmt.Fprintln(w, "URL:", r.URL)
-	// Method
-	fmt.Fprintln(w, "Method:", r.Method)
-	// Headers
-	fmt.Fprintln(w, "Headers:")
-	for headerKey, headerVal := range r.Header {
-		fmt.Fprintf(w, "  %v: %v\n", headerKey, headerVal)
-	}
-	// Query params...
-	fmt.Fprintln(w, "Params:")
-	for paramKey, paramVal := range r.URL.Query() {
-		fmt.Fprint(w, "  ", paramKey, ":")
-		for _, paramValItem := range paramVal {
-			fmt.Fprint(w, " ", paramValItem)
-		}
-		fmt.Fprintln(w)
-	}
-	// Body
-	data, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		log.Println("ERROR reading request body:", err)
-	} else {
-		fmt.Fprintln(w, "Body:", string(data))
-	}
+	httputils.DumpRequest(w, r)
 }
 
 func main() {
